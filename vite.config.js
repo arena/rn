c9e147cedf5162cfb9b9bf9482390ba4cb12432d
visualarena <info@visualarena.com>
2026-02-08 11:49:17 -0800

    green bg

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import yaml from '@rollup/plugin-yaml'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react(), yaml()],
-  base: '/rn/',
-})
+  base: command === 'build' ? '/rn/' : '/',
+}))
